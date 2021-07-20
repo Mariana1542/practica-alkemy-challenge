881761dc70ce2294dedf329532a753297f6efe77
Mariana <marianamillicovsky@gmail.com>
2021-07-20 20:27:38 -0300

    cambio

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,6 +1,6 @@
 module Api 
  module V1 
-   class CharacterController < ApplicationController
+   class CharactersController < ApplicationController
      def index
          characters = Character.all 
  
